a0c61844930aa35f782fefc2f934aa5a8e92c08d
Cody James Moores <codymoores@live.com>
2022-04-26 20:29:53 -0230

    Working Speech To Text with File Output using Vosk

---
+++ vosk_SP2T.py
@@ -0,0 +1,54 @@
+import time
+import speech_recognition as sr
+from vosk import Model
+
+r = sr.Recognizer()
+source = sr.Microphone()
+model = Model("model/")
+timestr = time.strftime("H(%H) - M(%M) - S(%S)")
+moment = time.strftime("%Y-%b-%d__%H_%M_%S", time.localtime())
+
+init = True
+while init:
+    # f = open('output'+moment+'.log', 'w')
+    f = open(moment+'.log', 'w')
+
+
+    try: 
+
+        with sr.Microphone() as source:
+            print("recording...")
+            r.adjust_for_ambient_noise(source, duration=0.2)
+            
+            audio2 = r.listen(source)
+            MyText = r.recognize_vosk(audio2)
+            MyText = MyText.lower()
+
+            if str is bytes:
+                result = u"{}".format(MyText).encode("utf-8")
+
+            else:
+                result = "{}".format(MyText)
+
+            print(MyText)
+            
+            # with open("outputs/output.txt", "a") as f:
+            f.write("\n")
+            f.write(timestr)
+            f.write("\n")
+            f.write(MyText)
+            f.write("\n")
+            print(result)                
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("Unknown error occured")
+    
+    except LookupError:  
+        print("Could not understand audio")
+
+    except KeyboardInterrupt:
+        print('\n ...stopped')
+        break
